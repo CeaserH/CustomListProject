5a73d1f38dcd8b788bf7948077a285abfc5806bd
CeaserH <ceaser.r.hernandez@gmail.com>
2021-02-23 08:26:40 -0800

    Added 3 more tests for Add. Including string tests.

--- CustomListTest/AddTests.cs
+++ CustomListTest/AddTests.cs
@@ -8,27 +8,31 @@ namespace CustomListTest
     {
         [TestMethod]
 
-        public void AddOneItemToList()
+
+        // Checking to see if list count is accurately increase by 1
+        public void AddOneIntToList()
         {
 
             CustomList<int> numbers = new CustomList<int>();
 
-            int ListCount = numbers.Count;
+            int ListCount = numbers.count;
 
             numbers.Add(1);
 
-            int sum = numbers.Count;
+            int sum = numbers.count;
 
             Assert.AreEqual(ListCount + 1, sum);
 
         }
 
-        public void AddFiveItemsToList()
+        // Checking to see if list count is accurately increased by 5
+
+        public void AddFiveIntsToList()
         {
 
             CustomList<int> numbers = new CustomList<int>();
 
-            int ListCount = numbers.Count;
+            int ListCount = numbers.count;
 
 
             numbers.Add(1);
@@ -38,11 +42,70 @@ namespace CustomListTest
             numbers.Add(5);
 
 
-            int sum = numbers.Count;
+            int sum = numbers.count;
 
             Assert.AreEqual(ListCount + 5, sum);
 
         }
+
+
+        // Checking to see if capacity increases from 4 once reached, should increase to 8
+        public void AddThreeStringsToList()
+        {
+
+            CustomList<string> stringTest = new CustomList<string>();
+
+            stringTest.Add("Lets");
+            stringTest.Add("test");
+
+            int listCap = stringTest.capacity;
+
+            stringTest.Add("this");
+            stringTest.Add("And see");
+            stringTest.Add("if it works.");
+
+            int newListCap = stringTest.capacity;
+
+            Assert.AreEqual(listCap + 3, newListCap);
+
+
+        }
+
+        // Checking to see if the correct string on index 0
+
+        public void IsStringInIndexZeroRight()
+        {
+
+            CustomList<string> stringTest = new CustomList<string>();
+
+
+            string testingIndex = stringTest.Add("Hello");
+
+            Assert.AreEqual("Hello", testingIndex);
+
+
+        }
+
+
+        // Checking to see if correct string on index provided
+
+        public void IsStringInIndexRight()
+        {
+
+            CustomList<string> stringTest = new CustomList<string>();
+
+            stringTest.Add("Hello,");
+            stringTest.Add("My name is,");
+            stringTest.Add("Ceaser");
+
+            var actual = stringTest[2];
+
+            Assert.AreEqual("Ceaser", actual);
+
+
+        }
+
+
 
 
     }
