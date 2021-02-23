9f8f2e32b6003055d41423400e186b74824b840e
CeaserH <ceaser.r.hernandez@gmail.com>
2021-02-23 07:11:53 -0800

    Added first addition test

---
+++ CustomListTest/AddTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CustomListTest
+{
+    [TestClass]
+    class AddTests
+    {
+        [TestMethod]
+
+        public void AddOne()
+        {
+
+            int NumOne = 1;
+            int NumTwo = 5;
+
+            CustomList<int> numbers = new CustomList<int>();
+            int sum = numbers.Add(NumOne, NumTwo);
+
+            Assert.AreEqual(6, sum);
+
+        }
+
+
+    }
+}
